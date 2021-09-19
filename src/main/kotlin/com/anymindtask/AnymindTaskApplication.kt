e843816b9217f8c39d4319766dc1d4d1a678786a
ardeshir <ardeshir.ahouri@gmail.com>
2021-09-19 12:53:16 +0430

    init commit

---
+++ src/main/kotlin/com/anymindtask/AnymindTaskApplication.kt
@@ -0,0 +1,11 @@
+package com.anymindtask
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AnymindTaskApplication
+
+fun main(args: Array<String>) {
+    runApplication<AnymindTaskApplication>(*args)
+}
---
+++ src/test/kotlin/com/anymindtask/AnymindTaskApplicationTests.kt
@@ -0,0 +1,15 @@
+package com.anymindtask
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+
+@SpringBootTest
+class AnymindTaskApplicationTests {
+
+    @Test
+    fun contextLoads() {
+
+    }
+
+}
